69e60c774d579bceaa2a646dc93c45d197d927cf
André Alves <a95033@alunos.uminho.pt>
2023-03-08 16:12:18 +0000

    TPC4 almost done

--- TPC4/main.py
+++ TPC4/main.py
@@ -1,41 +1,97 @@
 import re
+import json
+def process_list(str):
+    matches = re.findall(r"\d+", str)
+    return [int(m) for m in matches]
 
 
-def parse_csv(filename):
-    res = []
-    with open(filename, "r") as f:
-        lines = f.readlines()
-        for line in lines:
-            regex = re.split(r",", line)
-            # remove \n
-            regex[-1] = regex[-1].replace("\n", "")
-            print(regex)
-            res.append(regex)
-    return res
+def funct(lst, type):
+    if type == "media":
+        return sum(lst) / len(lst)
+
+    if type == "sum":
+        return sum(lst)
+
+    if type == "max":
+        return max(lst)
+
+    if type == "min":
+        return min(lst)
+
+    if type == "count":
+        return len(lst)
+
+    if type == "first":
+        return lst[0]
+
+    if type == "last":
+        return lst[-1]
+
+    if type == "median":
+        return sorted(lst)[len(lst) // 2]
 
 
-def to_json(lista):
-    with open("alunos1.json", "w") as f:
-        f.write("[\n")
-        for i in range(len(lista)):
-            f.write("{\n")
-            f.write(f"\"nome\": \"{list[i][0]}\",\n")
-            f.write(f"\"numero\": \"{list[i][1]}\",\n")
-            f.write(f"\"email\": \"{list[i][2]}\",\n")
-            f.write(f"\"nota\": \"{list[i][3]}\"\n")
-            if i != len(list) - 1:
-                f.write("},\n")
+def exercise(src_file, dest_file):
+    with open(src_file) as f:
+        # Remove line terminators from strings
+        csv = [s.strip() for s in f.readlines()]
+
+    header = re.findall(r"(\w+(?:\{\d+(?:,\d+)?\}(?:::\w+)?)?)[,;]?", csv[0])
+    lists = {}
+    aggregates = {}
+
+    for i in range(0, len(header)):
+        if match := re.search(r"(\w+)\{(\d+)(?:,(\d+))?\}(?:::(\w+))?", header[i]):
+            header[i] = match.group(1)
+
+            if match.group(3) is None:
+                lists[header[i]] = (int(match.group(2)), None)
             else:
-                f.write("}\n")
-        f.write("]\n")
-    return
+                lists[header[i]] = (int(match.group(2)), int(match.group(3)))
+
+            if match.group(4) is not None:
+                aggregates[header[i]] = match.group(4)
+    regex = ""
+    last = False
+    for item in header:
+        if item in lists:
+            last = True
+
+            quantity = f"{{{str(lists[item][0])}{'' if lists[item][1] is None else ',' + str(lists[item][1])}}}"
+            regex += f"(?P<{item}>([^,;]+[,;]?){quantity})"
+        else:
+            last = False
+            regex += rf"(?P<{item}>[^,;]+)[,;]"
+
+    # Remove last commas and add new line
+    if not last:
+        regex = regex[:-4]
+
+    data = []
+    for line in csv[1:]:
+        matches = re.finditer(regex, line)
+
+        for match in matches:
+            data += [match.groupdict()]
+
+    for i in range(0, len(data)):
+        for k in data[i]:
+            if k in lists:
+                data[i][k] = process_list(data[i][k])
+            if k in aggregates:
+                data[i][k] = funct(data[i][k], aggregates[k])
+    for k in aggregates:
+        for i in range(0, len(data)):
+            data[i][f"{k}_{aggregates[k]}"] = data[i][k]
+            del data[i][k]
+
+    with open(dest_file, "w") as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)
 
 
 def main():
-    lista = parse_csv("alunos1.csv")
-    to_json(lista)
-    return
+    exercise("csv/alunos11.csv", "json/alunos11.json")
 
 
-if __name__ == "__main__":
+if __name__ != "main":
     main()
